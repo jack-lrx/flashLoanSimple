f3a1ba38f3b5688df37b4fa8e43e6d0b10873933
gavin <982338299@qq.com>
2025-09-15 21:00:32 +0800

    flashLoanSimple init

--- internal/adapter/sushiswap.go
+++ internal/adapter/sushiswap.go
@@ -3,12 +3,15 @@ package adapter
 import (
 	"context"
 	"errors"
-	goethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gavin/flashLoanSimple/internal/contract/sushiswapv2factory"
+	"github.com/gavin/flashLoanSimple/internal/contract/uniswapv2pair"
 	"math/big"
-	"strings"
+	"os"
 )
 
 // SushiSwapAdapter 负责与 SushiSwap 闪电贷合约进行交互
@@ -37,18 +40,36 @@ func NewSushiSwapAdapter(client *ethclient.Client, address string) *SushiSwapAda
 // 参数 data: 附加数据
 // 返回值: 错误信息
 func (s *SushiSwapAdapter) FlashLoan(ctx context.Context, amount *big.Int, receiver common.Address, data []byte) error {
-
+	amount0Out := amount
+	amount1Out := big.NewInt(0)
+	chainID, err := s.client.NetworkID(ctx)
+	if err != nil {
+		return fmt.Errorf("get chainID failed: %v", err)
+	}
+	privKeyHex := os.Getenv("FLASHLOAN_PRIVKEY")
+	privKey, err := crypto.HexToECDSA(privKeyHex)
+	if err != nil {
+		return fmt.Errorf("invalid privkey: %v", err)
+	}
+	auth, err := bind.NewKeyedTransactorWithChainID(privKey, chainID)
+	if err != nil {
+		return fmt.Errorf("auth failed: %v", err)
+	}
+	auth.Context = ctx
+	pair, err := uniswapv2pair.NewUniswapv2pair(s.address, s.client)
+	if err != nil {
+		return fmt.Errorf("new pair instance failed: %v", err)
+	}
+	tx, err := pair.Swap(auth, amount0Out, amount1Out, receiver, data)
+	if err != nil {
+		return fmt.Errorf("swap failed: %v", err)
+	}
+	fmt.Printf("[UniswapV2 FlashLoan] tx sent: %s\n", tx.Hash().Hex())
 	return nil
 }
 
-// SushiSwapV2PairABI 与 Uniswap V2 相同
-const SushiSwapV2PairABI = `[{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"reserve0","type":"uint112"},{"internalType":"uint112","name":"reserve1","type":"uint112"},{"internalType":"uint32","name":"blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"}]`
-
 // SushiSwapV2Factory 地址（主网）
 const SushiSwapV2FactoryAddress = "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"
-
-// SushiSwapV2Factory ABI（只用 getPair 方法）
-const SushiSwapV2FactoryABI = `[{"constant":true,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]`
 
 // GetPrice 查询 SushiSwap 上 tokenA/tokenB 的价格（主网V2实现）
 // 参数 ctx: 上下文
@@ -56,56 +77,29 @@ const SushiSwapV2FactoryABI = `[{"constant":true,"inputs":[{"internalType":"addr
 // 返回值: 价格（单位：wei），错误信息
 func (s *SushiSwapAdapter) GetPrice(ctx context.Context, tokenA, tokenB string) (*big.Int, error) {
 	factoryAddr := common.HexToAddress(SushiSwapV2FactoryAddress)
-	factoryABI, err := abi.JSON(strings.NewReader(SushiSwapV2FactoryABI))
+	factory, err := sushiswapv2factory.NewSushiswapv2factory(factoryAddr, s.client)
 	if err != nil {
 		return nil, err
 	}
 	tokenAAddr := common.HexToAddress(tokenA)
 	tokenBAddr := common.HexToAddress(tokenB)
-	input, err := factoryABI.Pack("getPair", tokenAAddr, tokenBAddr)
+	pairAddr, err := factory.GetPair(nil, tokenAAddr, tokenBAddr)
 	if err != nil {
 		return nil, err
 	}
-	msg := goethereum.CallMsg{
-		To:   &factoryAddr,
-		Data: input,
-	}
-	result, err := s.client.CallContract(ctx, msg, nil)
-	if err != nil {
-		return nil, err
-	}
-	outputs, err := factoryABI.Unpack("getPair", result)
-	if err != nil || len(outputs) == 0 {
-		return nil, errors.New("getPair unpack failed")
-	}
-	pairAddr := outputs[0].(common.Address)
 	if pairAddr == (common.Address{}) {
 		return nil, errors.New("pair not found")
 	}
-	pairABI, err := abi.JSON(strings.NewReader(SushiSwapV2PairABI))
+	pair, err := uniswapv2pair.NewUniswapv2pair(pairAddr, s.client)
 	if err != nil {
 		return nil, err
 	}
-	input2, err := pairABI.Pack("getReserves")
+	reserves, err := pair.GetReserves(nil)
 	if err != nil {
 		return nil, err
 	}
-	msg2 := goethereum.CallMsg{
-		To:   &pairAddr,
-		Data: input2,
-	}
-	result2, err := s.client.CallContract(ctx, msg2, nil)
-	if err != nil {
-		return nil, err
-	}
-	outputs2, err := pairABI.Unpack("getReserves", result2)
-	if err != nil || len(outputs2) < 2 {
-		return nil, errors.New("getReserves unpack failed")
-	}
-	reserve0 := outputs2[0].(*big.Int)
-	reserve1 := outputs2[1].(*big.Int)
-	// SushiSwap V2: pair 的 token0/token1 按地址排序
-	// reserve0 对应 token0，reserve1 对应 token1
+	reserve0 := reserves.Reserve0
+	reserve1 := reserves.Reserve1
 	if tokenAAddr.Hex() < tokenBAddr.Hex() {
 		if reserve0.Cmp(big.NewInt(0)) == 0 {
 			return nil, errors.New("zero reserve0")
